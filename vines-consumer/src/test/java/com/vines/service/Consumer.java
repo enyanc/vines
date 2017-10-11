5418b641fff590a512a4f54c92df05d7f58584f9
enyanc <yesuailp@126.com>
2017-10-11 15:10:47 +0800

    1.增加了消费者模块 2.父pom文件增加了dependencyManagement元素，子模块可以通过手动添加相应的依赖 3.消费者模块目前日志打印不出来

---
+++ vines-consumer/src/test/java/com/vines/service/Consumer.java
@@ -0,0 +1,20 @@
+package com.vines.service;
+
+import com.vines.service.IHelloWorldService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by ken.lj on 2017/7/31.
+ */
+public class Consumer {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:spring/applicationContext-root.xml"});
+        context.start();
+
+        IHelloWorldService demoService = (IHelloWorldService) context.getBean("demoService"); // 获取远程服务代理
+        String hello = demoService.sayHello();// 执行远程方法
+
+        System.out.println(hello); // 显示调用结果
+    }
+}
