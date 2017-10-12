dad6c2a61632989cd3143d4d9c2972fdb36110a9
enyanc <yesuailp@126.com>
2017-10-12 23:30:18 +0800

    dubbo-rest的jar包所依赖的和dubbo2.5.5中的jar应该是冲突了.

--- vines-consumer/src/test/java/com/vines/service/Consumer.java
+++ vines-consumer/src/test/java/com/vines/service/Consumer.java
@@ -9,12 +9,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Consumer {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:spring/applicationContext-dubbo-consumer.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:spring/applicationContext-root.xml"});
         context.start();
 
         IHelloWorldService demoService = (IHelloWorldService) context.getBean("demoService"); // 获取远程服务代理
         String hello = demoService.sayHello();// 执行远程方法
 
-        System.out.println(hello); // 显示调用结果
+        System.out.println(hello+"============================="); // 显示调用结果
     }
 }
