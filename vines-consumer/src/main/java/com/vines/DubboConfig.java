ae44954563afd8a9d5ec604c0725cf462f330aeb
enyanc <yesuailp@126.com>
2017-10-16 16:33:37 +0800

    消费者模块增加了spring boot的方式

---
+++ vines-consumer/src/main/java/com/vines/DubboConfig.java
@@ -0,0 +1,11 @@
+package com.vines;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({"classpath:spring/applicationContext-root.xml"})
+public class DubboConfig {
+
+
+}
--- vines-consumer/src/main/java/com/vines/service/rest/RestUserServiceImp.java
+++ vines-consumer/src/main/java/com/vines/service/rest/RestUserServiceImp.java
@@ -1,22 +1,20 @@
 package com.vines.service.rest;
 
-import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import com.vines.service.IHelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Path("users")
-@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
-@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
+@RestController
 public class RestUserServiceImp implements RestUserService {
 
-    @GET
-    @Path("sayUser")
-    @Override
+    @Autowired
+    IHelloWorldService demoService;
+
+    @RequestMapping("/")
     public String sayUser() {
-        return " hello sayUser";
+        return demoService.sayHello();
     }
+
 }
