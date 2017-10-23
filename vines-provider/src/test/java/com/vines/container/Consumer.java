705c2e78aa8958a802fb77763dc91a2770bf89fc
enyanc <yesuailp@126.com>
2017-10-23 17:16:49 +0800

    测试了如果提供者返回list集合的，dubbo是可以序列化的

--- vines-provider/src/test/java/com/vines/container/Consumer.java
+++ vines-provider/src/test/java/com/vines/container/Consumer.java
@@ -2,6 +2,9 @@ package com.vines.container;
 
 import com.vines.service.IHelloWorldService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.vines.mybatis.pojo.User;
+
+import java.util.List;
 
 /**
  * Created by ken.lj on 2017/7/31.
@@ -13,8 +16,8 @@ public class Consumer {
         context.start();
 
         IHelloWorldService demoService = (IHelloWorldService) context.getBean("demoService"); // 获取远程服务代理
-        String hello = demoService.sayHello();// 执行远程方法
+        List<User> list = demoService.sayUser();// 执行远程方法
 
-        System.out.println(hello); // 显示调用结果
+        System.out.println(list+"=============="); // 显示调用结果
     }
 }
